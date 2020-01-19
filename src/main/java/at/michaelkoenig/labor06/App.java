d84eb066ad61393ecc8c86397e4655047a3f5287
Michael König <koenigmichael2002@gmail.com>
2020-01-19 15:52:01 +0100

    Finish GUI

--- src/main/java/at/michaelkoenig/labor06/App.java
+++ src/main/java/at/michaelkoenig/labor06/App.java
@@ -6,7 +6,14 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Properties;
+import java.util.stream.Collectors;
 
 /**
  * JavaFX App
@@ -14,12 +21,27 @@ import java.io.IOException;
 public class App extends Application {
 
     private static Scene scene;
+    private static SchuelerDAO dao;
 
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage stage) throws IOException, SQLException {
+        dao = new SchuelerDAO();
+        Properties props = new Properties();
+        props.load(new FileInputStream("connection.properties"));
+        dao.connect(props.getProperty("DB_URL"), props.getProperty("DB_USER"), props.getProperty("DB_PASSWORD"));
+        dao.setup();
+        List<Schueler> schueler = Files.lines(Paths.get("schueler.csv")).map(Schueler::fromCSVString)
+                .distinct().collect(Collectors.toList());
+        dao.persistSchueler(schueler);
+
         scene = new Scene(loadFXML("App"), 640, 480);
         stage.setScene(scene);
         stage.show();
+    }
+
+    @Override
+    public void stop() throws Exception {
+        dao.close();
     }
 
     static void setRoot(String fxml) throws IOException {
@@ -29,6 +51,14 @@ public class App extends Application {
     private static Parent loadFXML(String fxml) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
         return fxmlLoader.load();
+    }
+
+    public static SchuelerDAO getDao() {
+        return dao;
+    }
+
+    public static void setDao(SchuelerDAO dao) {
+        App.dao = dao;
     }
 
     public static void main(String[] args) {
--- src/main/java/at/michaelkoenig/labor06/AppController.java
+++ src/main/java/at/michaelkoenig/labor06/AppController.java
@@ -2,7 +2,10 @@ package at.michaelkoenig.labor06;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.scene.Node;
 import javafx.scene.control.ListView;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
@@ -11,29 +14,102 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 
 import java.net.URL;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
+import java.util.stream.Collectors;
 
 public class AppController implements Initializable {
 
     public ToggleGroup grpFilter;
+    public ToggleGroup grpGeschlecht = new ToggleGroup();
     public RadioButton radSchuelerzahlen;
     public RadioButton radKlasse;
     public RadioButton radGeschlecht;
-    public ListView listview;
+    public RadioButton radMaennlich = new RadioButton("Männlich");
+    public RadioButton radWeiblich = new RadioButton("Weiblich");
+    public ListView<String> listview;
     public GridPane gridPane;
     public TextField tfKlasse = new TextField();
     public HBox hbox;
 
-    private ObservableList obsList = FXCollections.observableArrayList();
+    private ObservableList<String> obsList = FXCollections.observableArrayList();
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         listview.setItems(obsList);
+        radMaennlich.setToggleGroup(grpGeschlecht);
+        radMaennlich.setPadding(new Insets(0, 50, 0, 0));
+        radWeiblich.setToggleGroup(grpGeschlecht);
 
+        setupListeners();
+        radKlasse.fireEvent(new ActionEvent());
+    }
+
+    private void setupListeners() {
         radKlasse.setOnAction(actionEvent -> {
-            hbox.getChildren().removeAll();
-            hbox.getChildren().add(tfKlasse);
+            setHboxElement(tfKlasse);
+            tfKlasse.fireEvent(new ActionEvent());
         });
 
+        tfKlasse.setOnAction(actionEvent1 -> {
+            klasseHandler();
+        });
+
+        radGeschlecht.setOnAction(actionEvent -> {
+            setHboxElement(radMaennlich, radWeiblich);
+            radMaennlich.setSelected(true);
+            geschlechtHandler();
+        });
+
+        radMaennlich.setOnAction(actionEvent -> {
+            geschlechtHandler();
+        });
+
+        radWeiblich.setOnAction(actionEvent -> {
+            geschlechtHandler();
+        });
+
+        radSchuelerzahlen.setOnAction(actionEvent -> {
+            hbox.getChildren().clear();
+            schuelerZahlenHandler();
+        });
     }
+
+    private void setHboxElement(Node... nodes) {
+        hbox.getChildren().clear();
+        hbox.getChildren().addAll(nodes);
+    }
+
+    private void klasseHandler() {
+        try {
+            List<Schueler> schueler = App.getDao().schuelerInKlasse(tfKlasse.getText());
+            obsList.setAll(schueler.stream().map(s -> s.getVorname() + " " + s.getNachname() + ", " + s.getKlasse()).collect(Collectors.toList()));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void geschlechtHandler() {
+        try {
+            char geschlecht = radMaennlich.isSelected() ? 'M' : 'W';
+            List<Schueler> schueler = App.getDao().schuelerNachGeschlecht(geschlecht);
+            obsList.setAll(schueler.stream().map(s -> s.getVorname() + " " + s.getNachname() + ", " + s.getKlasse()).collect(Collectors.toList()));
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void schuelerZahlenHandler() {
+        try {
+            Map<String, Integer> klassen = App.getDao().getKlassen();
+            obsList.setAll(klassen.entrySet().stream().map(s -> s.getKey() + ": " + s.getValue()).collect(Collectors.toList()));
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
